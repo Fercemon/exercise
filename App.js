0b99b1b4c196c02bd04ed3a5a66d34a14f272e21
Fercemon <38923003+Fercemon@users.noreply.github.com>
2019-09-30 12:32:51 -0400

    animation added

--- App.js
+++ App.js
@@ -1,33 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, NativeModules, Platform, Animated, TextInput } from 'react-native';
 import Svg, { Path, Rect, Circle, Line, Ellipse } from 'react-native-svg';
 import Robot from './robot';
 
+
 import robotsJSON from './robots.json';
 
- const { UIManager } = NativeModules;
+const { UIManager } = NativeModules;
 if (Platform.OS === 'android') {
  if (UIManager.setLayoutAnimationEnabledExperimental) {
    UIManager.setLayoutAnimationEnabledExperimental(true);
  }
-} 
+}  
 
 
 export default class App extends React.Component {
+
+  text1 = new Animated.Value(1)
+  text2 = new Animated.Value(0)
 
   state = { robots: []}
 
   componentDidMount() {
     this.setState({robots: robotsJSON})
-  }
+    Animated.timing(this.text1, {
+      toValue: 0,
+      duration: 2000
+    }).start()
+
+    Animated.timing(this.text2, {
+      toValue: 1,
+      delay: 2000,
+      duration: 2000
+    }).start()
+  } 
 
   render() { 
     return (
       <ScrollView contentContainerStyle={styles.container}>
-        <Text style={styles.title}>Hey, here yo have some robots</Text>
+        <Animated.Text style={{...styles.title,
+          transform: [{
+            translateX: this.text1.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 1.2]
+            })
+          }, {
+            translateY: this.text1.interpolate({
+              inputRange: [0, 1],
+              outputRange: [30, 0]
+            })
+          }]
+        }}>Here are some robots</Animated.Text>
+
         <View style={styles.cardsContainer}>
         {
           this.state.robots.map((robot, index) => 
+          <Animated.View style={{
+          transform: [{
+            rotate: this.text1.interpolate({
+              inputRange: [0, 1],
+              outputRange: ['0deg', '360deg']
+            })
+          },
+          {
+            scale: this.text2.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 1.2]
+            })
+          }]
+        }}>
           <Robot
             key={index} 
             face={robot.face}
@@ -40,7 +81,7 @@ export default class App extends React.Component {
             teeth={robot.teeth}
             name={robot.name}
             age={robot.age}>
-          </Robot>
+          </Robot></Animated.View>
         
           )
         }
